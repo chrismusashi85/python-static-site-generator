f645439fa74828bb6e32d9c00b3f27a18d665061
Hidenobu Fushimi <hidenobu.fushimi@tri-ad.global>
2021-03-11 17:40:14 +0900

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 
-class Site():
+class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        
-
